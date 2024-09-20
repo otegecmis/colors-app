875e53e2979cccd399e3fc9abac5ea5c4bbfcb3d
otegecmis <177798054+otegecmis@users.noreply.github.com>
2024-09-20 15:43:16 +0300

    chore: create a `project`

---
+++ colors-app/Controller/MainTabController.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+class MainTabController: UIViewController {
+    
+    // MARK: - Lifecycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    // MARK: - Helpers
+    func configureUI() {
+        view.backgroundColor = .systemRed
+    }
+}
