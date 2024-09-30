adace525d2f66d99d8f5e51534b222f0260c7c08
otegecmis <177798054+otegecmis@users.noreply.github.com>
2024-09-30 17:03:14 +0300

    feat: create custom `alert` view controller

--- colors-app/Utils/Extensions.swift
+++ colors-app/Utils/Extensions.swift
@@ -1,8 +1,25 @@
 import UIKit
 
+// MARK: - UIViewController
+extension UIViewController {
+    
+    // MARK: - Helpers
+    func presentAlertOnMainThread(title: String, message: String, buttonTitle: String, completion: (() -> Void)? = nil) {
+        DispatchQueue.main.async {
+            let alertVC = AlertViewController(title: title, message: message, buttonTitle: buttonTitle)
+            
+            alertVC.modalPresentationStyle = .overFullScreen
+            alertVC.modalTransitionStyle = .crossDissolve
+            alertVC.completion = completion
+            self.present(alertVC, animated: true)
+        }
+    }
+}
+
 // MARK: - UITabBarController
 extension UITabBarController {
     
+    // MARK: - Helpers
     func navigationTabController(title: String, image: UIImage?, rootViewController: UIViewController) -> UINavigationController {
         let nav = UINavigationController(rootViewController: rootViewController)
         let appearance = UITabBar.appearance()
@@ -19,6 +36,8 @@ extension UITabBarController {
 
 // MARK: - UIButton
 extension UIButton {
+    
+    // MARK: - Helpers
     func attributedTitle(firstPart: String) {
         let atts: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor.systemBrown, .font: UIFont.systemFont(ofSize: 16)]
         let attributedTitle = NSMutableAttributedString(string: "\(firstPart) ", attributes: atts)
---
+++ colors-app/Views/Alert/AlertButton.swift
@@ -0,0 +1,36 @@
+import UIKit
+
+class AlertButton: UIButton {
+    
+    // MARK: - Initializers
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init() {
+        super.init(frame: .zero)
+        
+        self.backgroundColor = .systemBrown
+        self.setTitle("Done", for: .normal)
+        configure()
+    }
+    
+    // MARK: - Helpers
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        layer.cornerRadius = 10
+        setTitleColor(.white, for: .normal)
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+    }
+    
+    func set(backgroundColor: UIColor, title: String) -> Void {
+        self.backgroundColor = backgroundColor
+        setTitle(title, for: .normal)
+    }
+}
