4788ed407a98d13546483acca7482be684149333
otegecmis <177798054+otegecmis@users.noreply.github.com>
2024-10-09 15:45:40 +0300

    feat: create `create color` functionality

--- colors-app/Controller/CreateController.swift
+++ colors-app/Controller/CreateController.swift
@@ -1,16 +1,71 @@
 import UIKit
 
-class CreateController: UIViewController {
+class CreateController: UIViewController, UIColorPickerViewControllerDelegate {
+    
+    // MARK: - Properties
+    private var selectedColorView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemGray
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
+    }()
+    
+    private var selectedColor: UIColor? = .systemGray6
     
     // MARK: - Lifecycles
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        configureViewController()
         configureUI()
     }
     
     // MARK: - Helpers
-    func configureUI() {
-        view.backgroundColor = .systemBackground
+    private func configureViewController() {
+        view.backgroundColor = .white
+        
         title = "Create"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(handleCreateButton))
+    }
+    
+    private func configureUI() {
+        view.addSubview(selectedColorView)
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(openColorPicker))
+        
+        selectedColorView.addGestureRecognizer(tapGesture)
+        selectedColorView.isUserInteractionEnabled = true
+        
+        NSLayoutConstraint.activate([
+            selectedColorView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 5),
+            selectedColorView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 10),
+            selectedColorView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: -10),
+            selectedColorView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -10)
+        ])
+        
+        selectedColorView.layer.cornerRadius = 10
+    }
+    
+    func colorPickerViewControllerDidSelectColor(_ viewController: UIColorPickerViewController) {
+        selectedColor = viewController.selectedColor
+        selectedColorView.backgroundColor = selectedColor
+    }
+    
+    // MARK: - Actions
+    @objc func openColorPicker() {
+        let colorPicker = UIColorPickerViewController()
+        
+        colorPicker.delegate = self
+        present(colorPicker, animated: true, completion: nil)
+    }
+    
+    @objc func handleCreateButton() {
+        if let color = selectedColor {
+            let hexString = color.toHexString()
+            print("HEX: \(hexString)")
+        } else {
+            print("ERROR: No color selected.")
+        }
     }
 }
--- colors-app/Controller/MainTabController.swift
+++ colors-app/Controller/MainTabController.swift
@@ -11,6 +11,17 @@ class MainTabController: UITabBarController {
         }
     }
     
+    lazy var floatingButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.tintColor = .white
+        button.backgroundColor = .label
+        button.setImage(UIImage(systemName: "paintbrush"), for: .normal)
+        
+        button.addTarget(self, action: #selector(handleFloatingButton), for: .touchUpInside)
+        
+        return button
+    }()
+    
     // MARK: - Lifecycles
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,7 +41,7 @@ class MainTabController: UITabBarController {
     // MARK: - Helpers
     func checkIfUserIsLoggedIn() {
         let currentUser = Auth.auth().currentUser
-                
+        
         if currentUser  == nil {
             DispatchQueue.main.async {
                 let signInController = SignInController()
@@ -44,18 +55,41 @@ class MainTabController: UITabBarController {
     
     func configureViewControllers(withUser user: User) {
         view.backgroundColor = .systemBackground
-
+        
         let profileController = ProfileController()
-
+        
         profileController.user = self.user
         profileController.tabBarItem.title = "Profile"
         
         let latest = navigationTabController(title: "Latest", image: UIImage(systemName: "paintpalette"), rootViewController: LatestController())
-        let create = navigationTabController(title: "Create", image: UIImage(systemName: "paintbrush"), rootViewController: CreateController())
         let random = navigationTabController(title: "Random", image: UIImage(systemName: "swatchpalette"), rootViewController: RandomController())
         let profile = navigationTabController(title: "Profile", image: UIImage(systemName: "theatermask.and.paintbrush"), rootViewController: profileController)
         
-        viewControllers = [latest, create, random, profile]
+        viewControllers = [latest, random, profile]
+        
+        view.addSubview(floatingButton)
+        floatingButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        floatingButton.heightAnchor.constraint(equalToConstant: 56).isActive = true
+        floatingButton.widthAnchor.constraint(equalToConstant: 56).isActive = true
+        floatingButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -65).isActive = true
+        floatingButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -16).isActive = true
+        floatingButton.layer.cornerRadius = 56 / 2
+    }
+    
+    // MARK: - Actions
+    @objc private func handleFloatingButton() {
+        let createController = CreateController()
+        let navigationController = UINavigationController(rootViewController: createController)
+        
+        navigationController.modalPresentationStyle = .pageSheet
+        
+        if let sheet = navigationController.sheetPresentationController {
+            sheet.detents = [.medium(), .large()]
+            sheet.prefersGrabberVisible = true
+        }
+        
+        present(navigationController, animated: true, completion: nil)
     }
 }
 
