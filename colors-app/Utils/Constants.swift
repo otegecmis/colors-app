50f567336ca8ad996771a3d0bba0229f0dee4aa0
otegecmis <177798054+otegecmis@users.noreply.github.com>
2024-10-09 15:42:16 +0300

    chore: add `mock colors` for `latest` and `profile` screens

--- colors-app/Utils/Constants.swift
+++ colors-app/Utils/Constants.swift
@@ -2,3 +2,8 @@ import Firebase
 
 // MARK: - Firebase Collections
 let COLLECTION_USERS = Firestore.firestore().collection("users")
+
+// MARK: - Mock Variables
+let MOCK_COLORS: [UIColor] = [
+    .red, .blue, .green, .yellow, .purple, .orange, .brown, .magenta, .cyan, .blue, .green, .yellow, .purple, .orange, .brown, .magenta, .cyan, .blue, .green, .yellow, .purple, .orange, .brown, .magenta, .cyan, .blue, .green, .yellow, .purple, .orange, .brown, .magenta, .cyan, .magenta, .cyan
+]
