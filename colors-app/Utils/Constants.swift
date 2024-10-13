692cda916a147a611b226fbf1231203d54a43f33
otegecmis <177798054+otegecmis@users.noreply.github.com>
2024-10-13 17:26:11 +0300

    feat: update `app icon` and `remove` unused `mock colors`

--- colors-app/Utils/Constants.swift
+++ colors-app/Utils/Constants.swift
@@ -3,8 +3,3 @@ import Firebase
 // MARK: - Firebase Collections
 let COLLECTION_USERS = Firestore.firestore().collection("users")
 let COLLECTION_COLORS = Firestore.firestore().collection("colors")
-
-// MARK: - Mock Variables
-let MOCK_COLORS: [UIColor] = [
-    .red, .blue, .green, .yellow, .purple, .orange, .brown, .magenta, .cyan, .blue, .green, .yellow, .purple, .orange, .brown, .magenta, .cyan, .blue, .green, .yellow, .purple, .orange, .brown, .magenta, .cyan, .blue, .green, .yellow, .purple, .orange, .brown, .magenta, .cyan, .magenta, .cyan
-]
